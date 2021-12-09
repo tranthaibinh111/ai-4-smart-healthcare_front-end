27ed5dcffc04076f9d5e15bb1a4dc84076b9aa46
Binh Tran <tranthaibinh111@yahoo.com>
2021-12-10 03:22:14 +0700

    Finish to edit for showing the hope web

--- src/components/about-address/index.jsx
+++ src/components/about-address/index.jsx
@@ -1,20 +1,24 @@
+// #region React
+import React from 'react';
 import PropTypes from 'prop-types';
+// #endregion
 
-const AboutAddress = ({ data }) => {
-    return (
-        <div className='office-address-item'>
-            <h5>{data.title}</h5>
-            <ul>
-                {data.list.map((single, i) => {
-                    return <li key={i}>{single}</li>;
-                })}
-            </ul>
-        </div>
-    );
-};
+const AboutAddress = ({ data }) => (
+  <div className="office-address-item">
+    <h5>{data.title}</h5>
+    <ul>
+      {data.list.map((single, i) => <li key={i}>{single}</li>)}
+    </ul>
+  </div>
+);
 
+// #region Khai báo Props
 AboutAddress.propTypes = {
-    data: PropTypes.object,
+  data: PropTypes.object,
 };
 
+AboutAddress.defaultProps = {
+  data: null,
+};
+// #endregion
 export default AboutAddress;
